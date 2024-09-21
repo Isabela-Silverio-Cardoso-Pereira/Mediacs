323a6d3170f0fa2356ac91b35a8c84fbd77812e5
isabe <isabe@DESKTOP-7JLA0L9>
2024-09-21 12:29:34 -0300

    Adicionar arquivos de projeto.

---
+++ programa1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           double A = Convert.ToDouble(Console.ReadLine());
+           double B = Convert.ToDouble(Console.ReadLine());
+            double multiplicacao1 = A * 3.5;
+            double multiplicacao2 = B * 7.5;
+            double soma = multiplicacao1 + multiplicacao2;
+            double pesos = 3.5 + 7.5;
+            double MEDIA = soma / pesos;
+            Console.WriteLine(MEDIA.ToString("F4"));
+
+
+
+
+
+
+
+        }
+    }
+}
